501d750fed783e3b3ea6a5575f032c7785673a6e
jungjieun <je_eun2735@naver.com>
2023-03-29 19:50:44 +0900

    html 추가

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const http =require('http');
 const hostname = 'localhost';
 const port = 3000;
 
-const mysql = require('mysql')
+const mysql = require('mysql');
+const { title } = require('process');
 const db = mysql.createConnection({
 host : 'localhost',
 user : 'nodejs',
@@ -13,14 +14,25 @@ database : 'opentutorials'
 
 db.connect();
 
+let body = "";
+body += "<!DOCTYPE html>";
+body += "<html>";
+body += "<head>";
+body += "<title>Node.js</title>";
+body += "</head>"
+body += "<body>"
+body += "<h1>Login</h1>"
+body += "<button>login</button>"
+body += "</body>"
+body += "</html>"
 
 const server = http.createServer((req,res)=>{
-  db.query('SELECT * FROM topic', function (error, topic){
-    console.log(topic);
+  db.query('SELECT * FROM topic', function (error, topics){
+    console.log(topics);
+    
   });
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hi');
+  res.end(body);
 });
 
 server.listen(port, hostname, ()=>{
